11a569303f234d3d1f70852ef9bbaf3fdc6fa4d0
LastOne817 <lastone817@naver.com>
2017-05-22 05:36:51 +0900

    Changed names to HW4

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -38,7 +38,7 @@ int init_glfw() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   // Open a window and create its OpenGL context
-  window = glfwCreateWindow( 1024, 768, "Graphics hw3", NULL, NULL);
+  window = glfwCreateWindow( 1024, 768, "Graphics hw4", NULL, NULL);
   if( window == NULL ){
     fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\n" );
     getchar();
